5951112c4051c0d4c015a9ba720d0b15eb8da808
Arkin <arkin.terli@gmail.com>
2025-03-18 06:40:35 -0700

    Minor refactoring to make function calls in the Externals.cmake file more elegant and developer-friendly.

--- Externals/Externals.cmake
+++ Externals/Externals.cmake
@@ -14,23 +14,29 @@ include(ExternalProject)
 # ---------------------------------------------------------------------------------
 
 # Builds and installs external git projects.
-function(add_external_git_project lib_name git_repository git_tag cmake_project_args external_bin_dir build_type)
-    message(STATUS "Configuring External Project: ${lib_name}")
+function(add_external_git_project)
+    set(options)
+    set(oneValueArgs NAME GIT_REPOSITORY GIT_TAG EXTERNALS_BIN_DIR BUILD_TYPE)
+    set(multiValueArgs CMAKE_ARGS)
+    cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    message(STATUS "Configuring External Project: ${ARG_NAME}")
+    set(lib_dir "${ARG_EXTERNALS_BIN_DIR}/${ARG_NAME}")
+
     ExternalProject_Add(
-            ${lib_name}
-            GIT_REPOSITORY ${git_repository}
-            GIT_TAG        ${git_tag}
-            PREFIX        "${external_bin_dir}/${lib_name}/prefix"
-            SOURCE_DIR    "${external_bin_dir}/${lib_name}/src"
-            STAMP_DIR     "${external_bin_dir}/${lib_name}/stamp"
-            BINARY_DIR    "${external_bin_dir}/${lib_name}/build"
-            INSTALL_DIR   "${external_bin_dir}/${lib_name}/install"
-            DOWNLOAD_DIR  "${external_bin_dir}/${lib_name}/download"
-            LOG_DIR       "${external_bin_dir}/${lib_name}/log"
-            CMAKE_ARGS
-            -DCMAKE_BUILD_TYPE=${build_type}
+            ${ARG_NAME}
+            GIT_REPOSITORY  ${ARG_GIT_REPOSITORY}
+            GIT_TAG         ${ARG_GIT_TAG}
+            PREFIX          "${lib_dir}/prefix"
+            SOURCE_DIR      "${lib_dir}/src"
+            STAMP_DIR       "${lib_dir}/stamp"
+            BINARY_DIR      "${lib_dir}/build"
+            INSTALL_DIR     "${lib_dir}/install"
+            DOWNLOAD_DIR    "${lib_dir}/download"
+            LOG_DIR         "${lib_dir}/log"
+            CMAKE_ARGS      -DCMAKE_BUILD_TYPE=${ARG_BUILD_TYPE}
             -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-            ${cmake_project_args}       # Project Build Options
+            ${ARG_CMAKE_ARGS}
             LOG_CONFIGURE ON
             LOG_BUILD ON
             LOG_INSTALL ON
@@ -39,16 +45,24 @@ function(add_external_git_project lib_name git_repository git_tag cmake_project_
             LOG_TEST ON
             LOG_MERGED_STDOUTERR ON
             LOG_OUTPUT_ON_FAILURE ON
+            GIT_SUBMODULES_RECURSE ON
+            GIT_PROGRESS OFF
+            GIT_SHALLOW  ON
             BUILD_ALWAYS ON
     )
-    set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_CLEAN_FILES "${external_bin_dir}/${lib_name}")
-    include_directories(${external_bin_dir}/${lib_name}/install/include)
-    link_directories(${external_bin_dir}/${lib_name}/install/lib)
+
+    set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_CLEAN_FILES "${lib_dir}")
+
+    include_directories(${lib_dir}/install/include)
+    link_directories(${lib_dir}/install/lib)
 endfunction()
 
 # ---------------------------------------------------------------------------------
 # COMMON SETTINGS
 # ---------------------------------------------------------------------------------
+
+# Externals build and install folder.
+set(EXTERNALS_BINARY_DIR "${CMAKE_BINARY_DIR}/Externals")
 
 # Common cmake project settings for the external projects.
 set(EXTERNAL_COMMON_CMAKE_ARGS
@@ -60,43 +74,30 @@ set(EXTERNAL_COMMON_CMAKE_ARGS
         -DCMAKE_INSTALL_RPATH=${CMAKE_INSTALL_RPATH}
 )
 
-# Externals build and install folder.
-set(EXTERNALS_BINARY_DIR "${CMAKE_BINARY_DIR}/Externals")
-
 # ---------------------------------------------------------------------------------
 # AIX CPP
 # ---------------------------------------------------------------------------------
-set(EXTERNAL_AIX_CMAKE_ARGS
-        ${EXTERNAL_COMMON_CMAKE_ARGS}
-        # Project specific cmake args
-        -DAIX_BUILD_EXAMPLES=OFF
-        -DAIX_BUILD_TESTS=OFF
-        -DAIX_BUILD_STATIC=ON
-)
-
 add_external_git_project(
-        "aix_cpp"
-        "https://github.com/godrays/AIX.git"
-        "${EXTERNAL_AIX_VERSION}"
-        "${EXTERNAL_AIX_CMAKE_ARGS}"
-        "${EXTERNALS_BINARY_DIR}"
-        "Release"
+        NAME                aix_cpp
+        GIT_REPOSITORY      https://github.com/godrays/AIX.git
+        GIT_TAG             ${EXTERNAL_AIX_VERSION}
+        CMAKE_ARGS          ${EXTERNAL_COMMON_CMAKE_ARGS}
+                            -DAIX_BUILD_EXAMPLES=OFF
+                            -DAIX_BUILD_TESTS=OFF
+                            -DAIX_BUILD_STATIC=ON
+        EXTERNALS_BIN_DIR   ${EXTERNALS_BINARY_DIR}
+        BUILD_TYPE          Release
 )
 
 # ---------------------------------------------------------------------------------
 # DOCOPT CPP
 # ---------------------------------------------------------------------------------
-set(EXTERNAL_DOCOPT_CMAKE_ARGS
-        ${EXTERNAL_COMMON_CMAKE_ARGS}
-        # Project specific cmake args
-        -DBUILD_SHARED_LIBS=OFF
-)
-
 add_external_git_project(
-        "docopt_cpp"
-        "https://github.com/docopt/docopt.cpp.git"
-        "${EXTERNAL_DOCOPT_VERSION}"
-        "${EXTERNAL_DOCOPT_CMAKE_ARGS}"
-        "${EXTERNALS_BINARY_DIR}"
-        "Release"
+        NAME                docopt_cpp
+        GIT_REPOSITORY      https://github.com/docopt/docopt.cpp.git
+        GIT_TAG             ${EXTERNAL_DOCOPT_VERSION}
+        CMAKE_ARGS          ${EXTERNAL_COMMON_CMAKE_ARGS}
+                            -DBUILD_SHARED_LIBS=OFF
+        EXTERNALS_BIN_DIR   ${EXTERNALS_BINARY_DIR}
+        BUILD_TYPE          Release
 )
